61fcfc7048bec633def8a617ac6ae14b3f05f66b
PhiLong1st <philongnguyen17048@gmail.com>
2025-09-03 17:31:38 +0700

    fix: add dice type

--- Assets/Scripts/Dice/Dice.cs
+++ Assets/Scripts/Dice/Dice.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 public class Dice : MonoBehaviour
 {
   [SerializeField] private int value;
+  [SerializeField] private DiceType type;
   public int Value => value;
+  public DiceType Type => type;
 }
