a9586aa975224bd4097cd4cb96e751efeba382bd
PhiLong1st <philongnguyen17048@gmail.com>
2025-09-04 19:32:11 +0700

    feat: add dice prefabs

--- Assets/Scripts/DiceSlot.cs
+++ Assets/Scripts/DiceSlot.cs
@@ -7,7 +7,14 @@ public class DiceSlot : MonoBehaviour, IDropHandler
   public int Row { get; private set; }
   public int Col { get; private set; }
 
+  private RectTransform rectTransform;
+
   public event Action OnDiceDrop;
+
+  private void Awake()
+  {
+    rectTransform = GetComponent<RectTransform>();
+  }
 
   public void Initialize(int row, int col)
   {
@@ -17,12 +24,19 @@ public class DiceSlot : MonoBehaviour, IDropHandler
 
   public void OnDrop(PointerEventData eventData)
   {
-    if (eventData.pointerDrag != null)
-    {
-      eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-      var dice = eventData.pointerDrag.GetComponent<Dice>();
-      Debug.Log($"Drop a {dice.Value} dice at cell [{Row}, {Col}]");
-      OnDiceDrop?.Invoke();
-    }
+    var dragGO = eventData.pointerDrag;
+    if (!dragGO) return;
+
+    var draggedRect = dragGO.GetComponent<RectTransform>();
+    draggedRect.SetParent(rectTransform, worldPositionStays: false);
+    draggedRect.anchoredPosition = Vector2.zero;
+    draggedRect.localRotation = Quaternion.identity;
+    draggedRect.localScale = Vector3.one;
+
+    var dice = dragGO.GetComponent<Dice>();
+    if (dice) Debug.Log($"Drop a {dice.Value} dice at cell [{Row}, {Col}]");
+
+    OnDiceDrop?.Invoke();
   }
+
 }
